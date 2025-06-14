2238c5888387e49d8f67b623d8f3ba57010ff25d
Ryobi <robiatmaja7@gmail.com>
2025-06-15 00:41:45 +0700

    feat: Implement dynamic API base URL configuration based on server environment

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
-// config.php
-$apiBaseUrl = "https://ancatamacitraabadi.co.id/Constructo-Landing";
+// Auto base url for API, image, etc
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+  $apiBaseUrl = "http://localhost/Constructo-Landing";
+} else {
+  $apiBaseUrl = "https://ancatamacitraabadi.co.id/Constructo-Landing";
+}
 ?>
+<script>
+window.API_BASE_URL = "<?= $apiBaseUrl ?>";
+</script>
